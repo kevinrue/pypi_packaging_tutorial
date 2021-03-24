13133c9b91bc40d13550a54817983ecf11bc6ca9
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-03-24 12:43:32 +0000

    rename package uniquely for Test PyPI upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",  # Replace with your own username
+    name="example-pkg-kevinrue",  # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
